f5885e8036385d24b1fd247cc0e75f9a9d49a5d5
Marcin <marcin.macina@outlook.com>
2023-02-03 18:45:32 +0100

    22.4 Create Card test

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -2,9 +2,13 @@ package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreatedTrelloCard {
     @JsonProperty("id")
@@ -18,4 +22,10 @@ public class CreatedTrelloCard {
 
     @JsonProperty("badges")
     private CreatedTrelloBadges badges;
+
+    public CreatedTrelloCard(String id, String name, String shortUrl) {
+        this.id = id;
+        this.name = name;
+        this.shortUrl = shortUrl;
+    }
 }
